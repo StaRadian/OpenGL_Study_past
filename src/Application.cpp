704183aa559de0e231b1be1337dd5c67da2a8921
StaRadian <rheehosung0627@gmail.com>
2021-05-18 16:13:37 +0900

    for ubuntu and the cherno #20 17:00

--- src/Application.cpp
+++ src/Application.cpp
@@ -30,7 +30,7 @@ int main(void) {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -48,10 +48,10 @@ int main(void) {
 
     {
         float positions[] = {
-                -0.5f, -0.5f, 0.0f, 0.0f,//0
-                0.5f, -0.5f, 1.0f, 0.0f,//1
-                0.5f, 0.5f, 1.0f, 1.0f,//2
-                -0.5f, 0.5f, 0.0f, 1.0f//3
+                100.0f, 100.0f, 0.0f, 0.0f,//0
+                200.0f, 100.0f, 1.0f, 0.0f,//1
+                200.0f, 200.0f, 1.0f, 1.0f,//2
+                100.0f, 200.0f, 0.0f, 1.0f//3
         };
 
         unsigned int indices[] = {
@@ -72,14 +72,14 @@ int main(void) {
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 
-        Shader shader("res/shaders/Basic.shader");
+        Shader shader("../res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
         shader.SetUniformMat4f("u_MVP", proj);
 
-        Texture texture("res/textures/Postman_Kyle.PNG");
+        Texture texture("../res/textures/Postman_Kyle.PNG");
         texture.Bind();
         shader.SetUniform1i("u_Texture", 0);
 
